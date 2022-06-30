44d57b6331be1afe1bfb99292f55c2db7ab3faa4
Matheus Guimarães <mgguima95@gmail.com>
2022-06-30 00:04:51 -0300

    Exercise 1 Completed

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shares/dish';
 
-const DISHES : Dish[] = [
+export const DISHES : Dish[] = [
   {
     id: '0',
     name: 'Uthappizza',
