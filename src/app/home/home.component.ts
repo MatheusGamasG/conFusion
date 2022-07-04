0a4f5270df6f74dc65191da2718441c3eaf05b8b
Matheus Guimarães <mgguima95@gmail.com>
2022-07-04 16:01:44 -0300

    Second Assignment

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { Dish } from '../shares/dish';
 import { DishService } from '../services/dish.service';
 import { Promotion } from '../shares/promotion';
 import { PromotionService } from '../services/promotion.service';
+import { LeaderService } from '../services/leader.service';
+import { Leader } from '../shares/leader';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +15,16 @@ export class HomeComponent implements OnInit {
 
   dish : Dish | undefined;
   promotion : Promotion | undefined;
+  leader : Leader | undefined;
 
-  constructor(private dishService : DishService, private promoService : PromotionService) { }
+  constructor(private dishService : DishService,
+    private promoService : PromotionService,
+    private leaderService : LeaderService) { }
 
   ngOnInit(): void {
     this.dish = this.dishService.getFeaturedDish();
     this.promotion = this.promoService.getFeaturedPromotion();
+    this.leader = this.leaderService.getFeaturedLeader();
   }
 
 }
