445276ad957e28a88dcda3fe5f5d91e04cea7f5c
Matheus Guimarães <mgguima95@gmail.com>
2022-07-28 19:05:24 -0300

    Animations for rendering fetched items

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,20 @@ import { Promotion } from '../shares/promotion';
 import { PromotionService } from '../services/promotion.service';
 import { LeaderService } from '../services/leader.service';
 import { Leader } from '../shares/leader';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style' : 'display: block'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class HomeComponent implements OnInit {
 
